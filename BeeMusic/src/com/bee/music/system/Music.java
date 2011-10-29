94dc55f073cc516edf927356ff85ef5cae4e05c5
Naveen.A.N <naveen.ann@gmail.com>
2011-10-29 11:39:33 +0530

    Initial Commit.

---
+++ BeeMusic/src/com/bee/music/system/Music.java
@@ -0,0 +1,36 @@
+/**   
+ * License Agreement for BeeMusic
+ *
+ * Copyright (C) 2010 Naveen.A.N
+ *
+ * BeeMusic is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BeeMusic is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with OpenSearchServer. 
+ *  If not, see <http://www.gnu.org/licenses/>.
+ **/
+package com.bee.music.system;
+
+/**
+ * @author Naveen.A.N
+ *
+ */
+public class Music {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("BeeMusic Player");
+
+	}
+
+}
